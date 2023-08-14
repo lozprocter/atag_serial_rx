26c998b2f7d5b395f3f05e2939fe55638d224378
lozprocter <72624353+lozprocter@users.noreply.github.com>
2023-08-14 17:28:41 +0100

    print actual error

--- ATAG_jig_serialrx.py
+++ ATAG_jig_serialrx.py
@@ -10,33 +10,33 @@ BAUD_RATE = 9600
 
 def establish_connection(port):
     
-    try:
-        filesForDevice = listdir('/dev/') # put all device files into list[]
+    # try:
+    filesForDevice = listdir('/dev/') # put all device files into list[]
 
-        for line in filesForDevice: # run through all files
+    for line in filesForDevice: # run through all files
 
-            if sys.platform == 'darwin':
+        if sys.platform == 'darwin':
 
-                if (line[:12] == 'tty.usbmodem'): # look for...   
-                    print("hey")
-                    devicePort = line # take whole line (includes suffix address e.g. ttyACM0
-                    e = serial.Serial('/dev/' + str(devicePort), BAUD_RATE, timeout = 6, writeTimeout = 20) # assign
-
-            # FLAG: This if statement is only relevant in linux environment. 
-            # EITHER: USB Comms hardware
-            # if (line[:6] == 'ttyUSB' or line[:6] == 'ttyACM'): # look for prefix of known success (covers both Mega and Uno)
-            # OR: UART Comms hardware
-            elif line[:7] == port: # looks specifically for USB port that encoder is plugged into
-                
-                print(port)
-                
+            if (line[:12] == 'tty.usbmodem'): # look for...   
+                print("hey")
                 devicePort = line # take whole line (includes suffix address e.g. ttyACM0
                 e = serial.Serial('/dev/' + str(devicePort), BAUD_RATE, timeout = 6, writeTimeout = 20) # assign
-                
-        return e
 
-    except: 
-        print('No arduino connected')
+        # FLAG: This if statement is only relevant in linux environment. 
+        # EITHER: USB Comms hardware
+        # if (line[:6] == 'ttyUSB' or line[:6] == 'ttyACM'): # look for prefix of known success (covers both Mega and Uno)
+        # OR: UART Comms hardware
+        elif line[:7] == port: # looks specifically for USB port that encoder is plugged into
+            
+            print(port)
+            
+            devicePort = line # take whole line (includes suffix address e.g. ttyACM0
+            e = serial.Serial('/dev/' + str(devicePort), BAUD_RATE, timeout = 6, writeTimeout = 20) # assign
+            
+    return e
+
+    # except: 
+        # print('No arduino connected')
 
 def receiver(serial_obj): 
     if serial_obj.inWaiting():

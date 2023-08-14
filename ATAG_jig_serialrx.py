222434296be108613b55332732936b50f778ad28
lozprocter <72624353+lozprocter@users.noreply.github.com>
2023-08-14 17:39:01 +0100

    formatting changes

--- ATAG_jig_serialrx.py
+++ ATAG_jig_serialrx.py
@@ -64,18 +64,16 @@ while True:
         line = receiver(serial_obj_1)
         if line:
             f.write(str(line) + str(f"Run: {run_counter}"))
-            print(f"RUN: {run_counter}")
-            print(line)
+            print(f"RUN: {run_counter} {line}")
         
     with open("ATAG_CABLE_RESULTS_2.txt", "a") as f:
         line = receiver(serial_obj_2)
         if line:
             f.write(str(line) + str(f"Run: {run_counter}"))
-            print(f"RUN: {run_counter}")
-            print(line)
+            print(f"RUN: {run_counter} {line}")
+  
     with open("ATAG_CABLE_RESULTS_3.txt", "a") as f:
         line = receiver(serial_obj_3)
         if line:
             f.write(str(line) + str(f"Run: {run_counter}"))
-            print(f"RUN: {run_counter}")
-            print(line)
+            print(f"RUN: {run_counter} {line}")

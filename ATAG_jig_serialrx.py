7c83dba71e928b00b4981df28b1241db96accfab
lozprocter <72624353+lozprocter@users.noreply.github.com>
2023-08-15 16:42:30 +0100

    button pin defined outside loop

--- ATAG_jig_serialrx.py
+++ ATAG_jig_serialrx.py
@@ -55,8 +55,8 @@ serial_obj_3.flushInput()
 
 def run_timing():
     run_counter = 0
+    run_prompt = Button(2)
     while True:
-        run_prompt = Button(2)
         run_prompt.wait_for_press()
         run_counter += 1
     
